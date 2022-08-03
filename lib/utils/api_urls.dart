e799bd2b834d0af756041547dc4d37a092401018
lala iqbal <lala.khokhar@toptal.com>
2022-08-03 06:36:02 +0500

    dot env

--- lib/utils/api_urls.dart
+++ lib/utils/api_urls.dart
@@ -1,11 +1,11 @@
+import '../enviroment/pangea_env.dart';
+
 class ApiUrls {
-  static const String base_url = "https://staging.api.pangea.chat/api/v1";
-  static const String get_all_flags = base_url + "/language/list";
-  static const String create_user = base_url +"/account/create";
-  static const String validate_user =
+  static String base_url = PengeaEnv.base_url;
+  static String get_all_flags = base_url + "/language/list";
+  static String create_user = base_url + "/account/create";
+  static String validate_user =
       base_url + "/account/validate_user?pangea_user_id=";
-  static const String user_details =
+  static String user_details =
       base_url + "/account/get_user_access_token?pangea_user_id=";
-
 }
-
