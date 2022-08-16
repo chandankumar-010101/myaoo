55756a44bcd789f88d39c5889e25190b65dda1ee
Sangam Sharma <sangamsharma@INFRAs-MacBook-Air-5.local>
2022-08-16 16:57:07 +0530

    2 points

--- lib/pages/search/search_view.dart
+++ lib/pages/search/search_view.dart
@@ -45,6 +45,7 @@ class _SearchViewState extends State<SearchView> {
   @override
   Widget build(BuildContext context) {
     // userAgeDetails();
+    searchController.getClasses();
     int age = int.parse(box.read("age").toString());
     final server = widget.controller.genericSearchTerm?.isValidMatrixId ?? false
         ? widget.controller.genericSearchTerm!.domain
