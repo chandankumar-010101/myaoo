ba540f85755e2c6dd48dbef1a75ff38c6cd49a5c
lalakhokhar <lala.khokhar@toptal.com>
2022-08-18 18:37:00 +0500

    updated prepare-web.sh

--- scripts/prepare-web.sh
+++ scripts/prepare-web.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -ve
 rm -rf js/package
+rmdir /s js
+mkdir js
 cd js/ && curl -L 'https://gitlab.com/famedly/company/frontend/libraries/olm/-/jobs/artifacts/master/download?job=build:js' > olm.zip && cd ../
 cd js/ && unzip olm.zip && cd ../
 cd js/ && rm olm.zip && cd ../
