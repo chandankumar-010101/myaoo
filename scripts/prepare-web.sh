6639ee8a51aeb9a42f9e5afd3d72c18bf17ff601
lalakhokhar <lala.khokhar@toptal.com>
2022-08-18 18:53:09 +0500

    Missing js script update

--- scripts/prepare-web.sh
+++ scripts/prepare-web.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -ve
 rm -rf js/package
-rmdir /s js
-mkdir js
+DIR="js/";
+if [ ! -d "$DIR"] then
+    mkdir $DIR
+fi
 cd js/ && curl -L 'https://gitlab.com/famedly/company/frontend/libraries/olm/-/jobs/artifacts/master/download?job=build:js' > olm.zip && cd ../
 cd js/ && unzip olm.zip && cd ../
 cd js/ && rm olm.zip && cd ../
