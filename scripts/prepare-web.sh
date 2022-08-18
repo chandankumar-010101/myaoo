ce48b5a86d3b46347cc8d9f0590f73a59165a773
lalakhokhar <lala.khokhar@toptal.com>
2022-08-18 18:57:58 +0500

    removed if condition from sh

--- scripts/prepare-web.sh
+++ scripts/prepare-web.sh
@@ -1,10 +1,6 @@
 #!/bin/bash -ve
 rm -rf js/package
-DIR="js/";
-if [ ! -d "$DIR" ] 
-then
-    mkdir $DIR
-fi
+mkdir -p js
 cd js/ && curl -L 'https://gitlab.com/famedly/company/frontend/libraries/olm/-/jobs/artifacts/master/download?job=build:js' > olm.zip && cd ../
 cd js/ && unzip olm.zip && cd ../
 cd js/ && rm olm.zip && cd ../
