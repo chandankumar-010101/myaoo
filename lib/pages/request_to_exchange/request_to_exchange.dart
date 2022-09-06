02a155c58439956681d96e90b237e81f56b6d4d8
saurabhsingh <saurabh.singh@oodles.io>
2022-09-06 23:46:55 +0530

    Exchange ui

--- lib/pages/request_to_exchange/request_to_exchange.dart
+++ lib/pages/request_to_exchange/request_to_exchange.dart
@@ -1,26 +1,18 @@
-import 'dart:html';
-
-import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:collection/collection.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:future_loading_dialog/future_loading_dialog.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:matrix/matrix.dart';
 import 'package:pangeachat/config/environment.dart';
 import 'package:pangeachat/model/class_detail_model.dart';
 import 'package:pangeachat/services/services.dart';
-import 'package:pangeachat/utils/url_launcher.dart';
 import 'package:pangeachat/widgets/star_rating.dart';
 import 'package:vrouter/vrouter.dart';
-
 import '../../config/app_config.dart';
-import '../../widgets/avatar.dart';
 import '../../widgets/matrix.dart';
-
-import 'package:flutter_gen/gen_l10n/l10n.dart';
-
 import '../chat_list/spaces_entry.dart';
+import 'package:matrix/matrix.dart' as sdk;
 var box = GetStorage();
 class RequestToExchange extends StatefulWidget {
   const RequestToExchange({Key? key}) : super(key: key);
@@ -65,6 +57,10 @@ class _RequestToExchangeState extends State<RequestToExchange> {
   List<Room> get spaces =>
       Matrix.of(context).client.rooms.where((r) => r.isSpace).toList();
 
+
+
+
+
   @override
   void initState() {
     // TODO: implement initState
@@ -102,16 +98,22 @@ class _RequestToExchangeState extends State<RequestToExchange> {
   }
 }
 
-class RequestToExchangePopUp extends StatelessWidget {
+class RequestToExchangePopUp extends StatefulWidget {
   List<Room> spaces;
   String roomId;
   String receivedroomID;
   String id;
   String r_id;
+
   RequestToExchangePopUp(
       {Key? key, required this.spaces, required this.id,required this.r_id, required this.roomId,required this.receivedroomID})
       : super(key: key);
 
+  @override
+  State<RequestToExchangePopUp> createState() => _RequestToExchangePopUpState();
+}
+
+class _RequestToExchangePopUpState extends State<RequestToExchangePopUp> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -119,16 +121,16 @@ class RequestToExchangePopUp extends StatelessWidget {
     var data = basePath.split("/api/v1");
     String url = data[0];
 
-    List date1=id.split(":");
+    List date1=widget.id.split(":");
     List date2=date1[0].split("@");
 
 
 
 
-    List<Room> space = spaces.where((i) => i.id == receivedroomID).toList();
-    if (box.read("clientID")==r_id) {
+    List<Room> space = widget.spaces.where((i) => i.id == widget.receivedroomID).toList();
+    if (box.read("clientID")==widget.r_id) {
       return FutureBuilder(
-        future: PangeaServices.fetchExchangeClassInfo(context, receivedroomID),
+        future: PangeaServices.fetchExchangeClassInfo(context, widget.receivedroomID),
         builder: (context, snapshot) {
           if (snapshot.hasError) {
             return Center(child: Text(snapshot.error.toString()));
@@ -434,7 +436,7 @@ class RequestToExchangePopUp extends StatelessWidget {
                                       width: 20,
                                       height: 20,
                                       child: Image.network(url +
-                                          data.flags![0].languageFlag
+                                          data.flags[0].languageFlag
                                               .toString())),
                                 ]),
                                 const SizedBox(
@@ -469,7 +471,7 @@ class RequestToExchangePopUp extends StatelessWidget {
                                       width: 20,
                                       height: 20,
                                       child: Image.network(url +
-                                          data.flags![1].languageFlag
+                                          data.flags[0].languageFlag
                                               .toString())),
                                   const SizedBox(
                                     width: 5,
@@ -520,7 +522,7 @@ class RequestToExchangePopUp extends StatelessWidget {
                     ),
                   ),
                   box.read("usertype") == 2
-                      ? box.read("clientID") == id
+                      ? box.read("clientID") == widget.id
                       ? Container()
                       : Container(
                     margin: const EdgeInsets.symmetric(
@@ -555,31 +557,22 @@ class RequestToExchangePopUp extends StatelessWidget {
                                     .onPrimary,
                               ),
                             ),
-                            onPressed: () {
-                              final String id = context.vRouter
-                                  .queryParameters['id'] ??
-                                  "";
-                              final box = GetStorage();
-                              if (id.isNotEmpty) {
-                                box.write("public",
-                                    data.permissions!.isPublic);
-                                box.write(
-                                    "openEnrollment",
-                                    data.permissions!
-                                        .isOpenEnrollment);
-                                box.write(
-                                    "openExchange",
-                                    data.permissions!
-                                        .isOpenExchange);
-                                box.write("ExchangeClientID",
-                                    data.classAuthorId);
-                                context.vRouter.to(
-                                    "/classDetails/exchange_class",
-                                    queryParameters: {
-                                      "class_id": id,
-                                    });
-                              }
-                            },
+                            onPressed: ()async {
+
+
+                              final room = Matrix.of(context).client.getRoomById(widget.roomId);
+
+
+                              print(room!.requestParticipants().val.toString());
+
+
+
+
+
+                              var className="${room!.displayname}/${data.className}";
+
+                              box.write("className",className);
+                             },
                             child: Text(
                               "Confirm Exchange Request",
                               style: const TextStyle().copyWith(
@@ -618,12 +611,10 @@ class RequestToExchangePopUp extends StatelessWidget {
                               ),
                             ),
                             onPressed: () {
-                              UrlLauncher(context,
-                                  'https://matrix.to/#/${id.toString()}')
-                                  .openMatrixToUrl();
+                              PangeaServices.ExchangeRejectRequest(data.pangeaClassRoomId,data.classAuthorId);
                             },
                             child: Text(
-                              "Message ${date2}",
+                              "Cancel",
                               style: const TextStyle().copyWith(
                                   color: Theme.of(context)
                                       .textTheme
@@ -634,6 +625,48 @@ class RequestToExchangePopUp extends StatelessWidget {
                             ),
                           ),
                         ),
+                        // const SizedBox(
+                        //   width: 10,
+                        //   height: 10,
+                        // ),
+                        // SizedBox(
+                        //   width: 200,
+                        //   child: OutlinedButton(
+                        //     style: OutlinedButton.styleFrom(
+                        //       shape: RoundedRectangleBorder(
+                        //           borderRadius:
+                        //           BorderRadius.circular(
+                        //               25.0)),
+                        //       side: BorderSide(
+                        //         width: 2,
+                        //         color: Theme.of(context)
+                        //             .colorScheme
+                        //             .onPrimary ==
+                        //             Colors.white
+                        //             ? Theme.of(context)
+                        //             .primaryColor
+                        //             : Theme.of(context)
+                        //             .colorScheme
+                        //             .onPrimary,
+                        //       ),
+                        //     ),
+                        //     onPressed: () {
+                        //       UrlLauncher(context,
+                        //           'https://matrix.to/#/${id.toString()}')
+                        //           .openMatrixToUrl();
+                        //     },
+                        //     child: Text(
+                        //       "Message ${date2}",
+                        //       style: const TextStyle().copyWith(
+                        //           color: Theme.of(context)
+                        //               .textTheme
+                        //               .bodyText1!
+                        //               .color,
+                        //           fontWeight: FontWeight.w400,
+                        //           fontSize: 12),
+                        //     ),
+                        //   ),
+                        // ),
                       ],
                     ),
                   )
@@ -653,6 +686,101 @@ class RequestToExchangePopUp extends StatelessWidget {
       return const Center(
         child: Text("You are not authorized for this page."),
       );
+    }
+  }
+
+  submitAction() async {
+    try {
+      String className;
+      String cityName;
+      String countryName;
+      int languageLevel;
+      String schoolName;
+      String disc;
+      String targetLanguage;
+      String sourceLanguage;
+      bool publicGroup;
+      bool openEnrollment;
+      bool openToExchange;
+
+      try {
+        className = box.read("className");
+        // cityName = box.read("cityName");
+        // countryName = box.read("countryName");
+        // languageLevel = box.read("languageLevel");
+        // schoolName = box.read("scoolName");
+        // disc = box.read("disc");
+        // targetLanguage = box.read("targetLanguage");
+        // sourceLanguage = box.read("sourceLanage");
+        publicGroup = box.read("publicGroup");
+        // openEnrollment = box.read("openEnrollment");
+        // openToExchange = box.read("openToExchange");
+
+      } catch (e) {
+        if (kDebugMode) {
+          print("Unable to fetch Data from storage");
+          print(e);
+        }
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text("Unable to fetch Data from storage")));
+        return;
+      }
+      final matrix = Matrix.of(context);
+      final roomID = await showFutureLoadingDialog(
+        context: context,
+        future: () => matrix.client.createRoom(
+
+          preset: publicGroup
+              ? sdk.CreateRoomPreset.publicChat
+              : sdk.CreateRoomPreset.privateChat,
+          creationContent: {'type': RoomCreationTypes.mSpace},
+          visibility: publicGroup ? sdk.Visibility.public : null,
+          roomAliasName: publicGroup && className.isNotEmpty
+              ? className.trim().toLowerCase().replaceAll(' ', '_')
+              : null,
+          name: className.isNotEmpty ? className : null,
+        ),
+      );
+      if (roomID.result != null) {
+        if (kDebugMode) {
+          print(roomID.result);
+        }
+        // await PangeaServices.createClass(
+        //   context: context,
+        //   isPublic: publicGroup,
+        //   isSharePhoto: sharePhotos,
+        //   isCreateRooms: createRooms,
+        //   isShareFiles: shareFiles,
+        //   isShareVideo: shareVideos,
+        //   isCreateStories: createStories,
+        //   isShareLocation: shareLocation,
+        //   isOpenExchange: openToExchange,
+        //   oneToOneChatExchange: oneToOneChatsWithinExchanges,
+        //   isOpenEnrollment: openEnrollment,
+        //   isCreateRoomsExchange: createRoomsInExchanges,
+        //   oneToOneChatClass: oneToOneChatsWithinClass,
+        //   className: className.toString(),
+        //   languageLevel: languageLevel,
+        //   city: cityName.toString(),
+        //   roomId: roomID.result!,
+        //   desc: disc.toString(),
+        //   country: countryName,
+        //   dominantLanguage: sourceLanguage,
+        //   targetLanguage: targetLanguage,
+        //   schoolName: schoolName.toString(),
+        // );
+      }
+      if (roomID == null) {
+        VRouter.of(context).toSegments(['rooms', roomID.result!, 'details']);
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+      ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Something went wrong")));
+
+
     }
   }
 }
