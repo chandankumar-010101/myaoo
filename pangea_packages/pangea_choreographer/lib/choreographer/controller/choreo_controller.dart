5956275b2cf53584b9cc72750657761d64a692d6
lalaiqbaltoptal <lala.khokhar@toptal.com>
2022-09-07 06:51:31 +0500

    On edit close

--- pangea_packages/pangea_choreographer/lib/choreographer/controller/choreo_controller.dart
+++ pangea_packages/pangea_choreographer/lib/choreographer/controller/choreo_controller.dart
@@ -36,7 +36,7 @@ class ChoreoController {
     myMatrixClient = MyMatrixClient(this);
   }
 
-  String get originalText => textController!.value.text;
+  String get originalText => state!.originalText;
   setTextEditingController(TextEditingController textEditingController) {
     textController = textEditingController;
     _addClearOnEditListener();
@@ -90,6 +90,7 @@ class ChoreoController {
   void clearState() {
     state!.clearState();
     step2!.clearState();
+
     errorService!.clearState();
 
     setState();
@@ -133,13 +134,22 @@ class ChoreoController {
   }
 
   void _addClearOnEditListener() {
-    this.textController!.addListener(() {
-      if (this.textController!.value != originalText) {
-        print('Text Event');
-        if (!state!.isEditing) {
-          clearState();
-        }
+    if (!textController!.hasListeners) {
+      textController!.removeListener(_listernerFunction);
+    }
+    this.textController!.addListener(_listernerFunction);
+  }
+
+  _listernerFunction() {
+    if (this.textController!.value.text != originalText) {
+      print(originalText);
+      print(this.textController!.value.text);
+      print('Text Event');
+      if (!state!.isEditing) {
+        clearState();
       }
-    });
+    } else {
+      print('There is no change in value');
+    }
   }
 }
