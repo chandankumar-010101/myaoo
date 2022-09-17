648fcca4827256958a649a1fb5cc8230bc55eac2
lalaiqbaltoptal <lala.khokhar@toptal.com>
2022-09-17 15:54:09 +0500

    Re added api key

--- lib/utils/choreo_util.dart
+++ lib/utils/choreo_util.dart
@@ -2,6 +2,10 @@ import 'package:pangeachat/config/environment.dart';
 
 class ChoreoUtil {
   static Map<String, String>? get headers {
-    return {"Content-Type": "application/json", "Accept": "application/json"};
+    return {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+      'api_key': Environment.choreo_api_key
+    };
   }
 }
